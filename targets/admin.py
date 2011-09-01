a8ac35af3f1688d545fc1c33e7e9fb1e6bd40354
Andrew Kornilov <frutik@gmail.com>
2011-09-01 15:16:47 +0300

    small changes in targets admin

--- targets/admin.py
+++ targets/admin.py
@@ -2,9 +2,10 @@ from targets.models import MailTarget, TwitterTarget
 from django.contrib import admin
 
 class MailTargetAdmin(admin.ModelAdmin):
-    pass
+    list_display_links = list_display = ('title', 'enabled')
+    list_filter = ('enabled',)
 
-class TwitterTargetAdmin(admin.ModelAdmin):
+class TwitterTargetAdmin(MailTargetAdmin):
     pass
     
 admin.site.register(MailTarget, MailTargetAdmin)
