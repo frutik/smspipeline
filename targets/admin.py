5d23f3bfc30754fbd86aac78af8cd00b2f725a6a
Andrew Kornilov <frutik@gmail.com>
2011-09-01 15:33:44 +0300

    for targets added enable/disable mass action in admin interface

--- targets/admin.py
+++ targets/admin.py
@@ -5,6 +5,25 @@ class MailTargetAdmin(admin.ModelAdmin):
     list_display_links = list_display = ('title', 'enabled')
     list_filter = ('enabled',)
 
+    actions = ['disable_selected', 'enable_selected']
+
+    
+    def disable_selected(self, request, queryset):
+	self.set_enabled(request, queryset, enabled=False)
+
+    def enable_selected(self, request, queryset):
+	self.set_enabled(request, queryset, enabled=True)
+
+    def set_enabled(self, request, queryset, enabled):
+	for obj in queryset:
+            obj.enabled = enabled
+            obj.save()
+            
+        self.message_user(request, "%s targets were successfully updated" % len(queryset))
+
+    enable_selected.short_description = 'Enable selected targets'
+    disable_selected.short_description = 'Disable selected targets'
+
 class TwitterTargetAdmin(MailTargetAdmin):
     pass
     
