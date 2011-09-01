042fddc3ac64c9e943d851769e11dac0096e1062
Andrew Kornilov <frutik@gmail.com>
2011-09-01 15:01:05 +0300

    small code improvements

--- targets/models.py
+++ targets/models.py
@@ -22,31 +22,34 @@ class Target(models.Model):
         super(Target,self).save(*args, **kwargs)
 
 class MailTarget(Target):
+
     exact_type = MAIL
     email = models.EmailField(max_length=255)
 
 class TwitterTarget(Target):
+
     exact_type = TWITTER
     access_key = models.CharField(max_length=255)
     access_secret = models.CharField(max_length=255)
 
 class TargetRunner(object):
 
-    def set_options(self, options):
+    def __init__(self, target_options):
 	pass
-
+	
     def send(self,message):
+
 	print self, message
 
 class TwitterTargetRunner(TargetRunner):
 
-    #TODO constructor???
-    def set_options(self, target):
+    def __init__(self, target_options):
 
-	self.ACCESS_KEY = target.twittertarget.access_key
-	self.ACCESS_SECRET = target.twittertarget.access_secret
+	self.ACCESS_KEY = target_options.twittertarget.access_key
+	self.ACCESS_SECRET = target_options.twittertarget.access_secret
 
     def send(self,message):
+
 	print self, message
 
 	import tweepy, targets.credentials
@@ -68,10 +71,5 @@ class TargetRunnerFactory:
     }
         
     @staticmethod
-    def get_runner(target):
-	runner = TargetRunnerFactory.RUNNERS[target.kind]()
-
-	if runner:
-	    runner.set_options(target)
-	    
-	return runner
+    def get_runner(target_options):
+	return TargetRunnerFactory.RUNNERS[target_options.kind](target_options)
