29c5ec3aada790182d39505ffdd3de8f4c9cc396
Andrew Kornilov <frutik@gmail.com>
2011-09-02 22:51:35 +0300

    after becherovka fixes

--- signals.py
+++ signals.py
@@ -15,8 +15,7 @@ def create_stuff_on_activation(sender, user, request, **kwargs):
     template.title = u'simple template'
 #    regexp
 #    template
-
-    pass
+    template.save()
 
 user_activated.connect(login_on_activation)
 user_activated.connect(create_stuff_on_activation)
