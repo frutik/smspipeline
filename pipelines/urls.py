ecf4b13724326b4748a19cb081fec72fb234cffe
Andrew Kornilov <frutik@gmail.com>
2011-09-02 14:40:15 +0300

    added pipeline executoin log stub

--- pipelines/urls.py
+++ pipelines/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'pipelines.views.show_all', name='pipelines_show_all'),
+    url(r'log/$', 'pipelines.views.execution_log', name='pipelines_execution_log'),
 )
--- pipelines/views.py
+++ pipelines/views.py
@@ -9,3 +9,8 @@ def show_all(request):
     pipelines = Pipeline.objects.all()
 
     return render_to_response('pipelines/show_all.html', {'pipelines':pipelines}, RequestContext(request))
+
+@login_required
+def execution_log(request):
+
+    return render_to_response('pipelines/execution_log.html', {}, RequestContext(request))
