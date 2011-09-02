1cb66717561acb2b386ccc885317c59d50887880
Andrew Kornilov <frutik@gmail.com>
2011-09-02 10:52:42 +0300

    added twitter bootstrap

--- urls.py
+++ urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.shortcuts import redirect
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'app.views.home', name='home'),
     
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="auth_login"),
@@ -17,11 +14,5 @@ urlpatterns = patterns('',
     url(r'^targets/', include('targets.urls')),
     url(r'^pipelines/', include('pipelines.urls')),
     
-    # url(r'^django_tests/', include('django_tests.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
