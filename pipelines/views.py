9a50a7ea9e6fd7073ddf52e2d1058f480829b03d
Andrew Kornilov <frutik@gmail.com>
2011-09-02 12:36:10 +0300

    basic views added

--- pipelines/views.py
+++ pipelines/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from pipelines.models import Pipeline
 
 @login_required
 def show_all(request):
-    return render_to_response('pipelines/show_all.html', {}, RequestContext(request))
+
+    pipelines = Pipeline.objects.all()
+
+    return render_to_response('pipelines/show_all.html', {'pipelines':pipelines}, RequestContext(request))
--- targets/views.py
+++ targets/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from targets.models import Target
 
 @login_required
 def show_all(request):
-    return render_to_response('targets/show_all.html', {}, RequestContext(request))
+
+    targets = Target.objects.all()
+
+    return render_to_response('targets/show_all.html', {'targets':targets}, RequestContext(request))
