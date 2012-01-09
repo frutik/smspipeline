35f9520e8a891038492a91a3474279282db8bbfd
Andrew Kornilov <frutik@gmail.com>
2012-01-09 21:33:05 +0200

    fixed settings

--- web/settings-aws.py
+++ web/settings-aws.py
@@ -121,6 +121,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
+    'django.core.context_processors.static',
 
     # required by django-admin-tools
     'django.core.context_processors.request',
